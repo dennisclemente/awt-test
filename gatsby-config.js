254dc5961eb9cec338ebdcad934640a2d5f61b39
dennisclemente <dennisclemente@gmail.com>
2020-02-17 22:26:40 -0500

    fixed examples.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,20 @@
 
 module.exports = {
   /* Your site config here */
+siteMetadata: {
+  title: 'contest',
+  description: 'just some description',
+  author: '@dc',
+  data:{ name: 'John', age:35 },
+},
   plugins:[
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
     `gatsby-plugin-sass`,
     `gatsby-plugin-styled-components`
   ],
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,24 +1,29 @@
 import React from 'react'
 import { Link } from 'gatsby'
 // import styles from './navbar.module.scss'
-
 const Navbar = () => {
-    return (
-        <nav>
-        <h2>company</h2>
-            <ul>
-                <li>
-                <Link to="/">Home</Link>
-                </li>
-                <li>
-                <Link to="/blog">Blog</Link>
-                </li>
-                <li>
-                <Link to="/products">Products</Link>
-                </li>
-            </ul>
-        </nav>
-    )
+  return (
+    <nav>
+      <h2>company</h2>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/blog/">Blog</Link>
+        </li>
+        <li>
+          <Link to="/products/">Products</Link>
+        </li>
+        <li>
+          <Link to="/examples/">examples</Link>
+        </li>
+        <li>
+          <Link to="/images/">images</Link>
+        </li>
+      </ul>
+    </nav>
+  )
 }
 
 export default Navbar
